e4c191ca7d548ae8ddaef180c29565a590325483
rianreiss <rianreis2109@gmail.com>
2024-01-10 23:29:45 -0300

    first commit

---
+++ learning-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React.</h1>
+
+      <p>My first React.</p>
+    </div>
+  );
+}
+
+export default App;
