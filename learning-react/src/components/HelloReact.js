5caa73ece595cbfe181d62a1d24aa36a38b859b2
rianreiss <rianreis2109@gmail.com>
2024-01-14 13:57:09 -0300

    css on components

--- learning-react/src/components/HelloReact.js
+++ learning-react/src/components/HelloReact.js
@@ -1,9 +1,12 @@
+import styles from './HelloReact.module.css'
+
 function HelloReact() {
 
 
     return (
         <div>
             <h1>Hello React.</h1>
+            <p className={styles.testing}>Rian Reis</p>
         </div>
     )
 }
