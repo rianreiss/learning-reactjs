4f068bfcdeeafa24177e7418f081d4e057e4d1c3
rianreiss <rianreis2109@gmail.com>
2024-01-14 13:43:06 -0300

    testing

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloReact from './components/HelloReact'
 
 function App() {
+
+  const name = 'Rian'
+
+  const newName = name.toUpperCase()
+
   return (
     <div className="App">
       <h1>Hello React.</h1>
-
       <p>My first React.</p>
+
+      <p>Testing {name}, testing {newName}.</p>
+
+      <HelloReact />
+
     </div>
   );
 }
---
+++ learning-react/src/components/HelloReact.js
@@ -0,0 +1,11 @@
+function HelloReact() {
+
+
+    return (
+        <div>
+            <h1>Hello React.</h1>
+        </div>
+    )
+}
+
+export default HelloReact
